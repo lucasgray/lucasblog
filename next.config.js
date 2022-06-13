5006f669729d207fc4179e428b998fa5fff7c82f
lucas gray <lucas.e.gray@gmail.com>
2022-06-13 14:25:52 -0500

    include subdir

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *;
   connect-src *;
   font-src 'self';
-  frame-src googletagmanager.com
+  frame-src www.googletagmanager.com
 `
 
 const securityHeaders = [
