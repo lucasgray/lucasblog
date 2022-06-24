7d1ae7444890360410fab7a6005b058b9f022a71
lucas gray <lucas.e.gray@gmail.com>
2022-06-24 08:10:20 -0500

    add some projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,24 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Advent of the Reaper',
+    description: `A pixel Tactics RPG in the vein of the GBA Fire Emblem games, featuring upgraded graphics, strategies, and a great story and soundtrack! Developing as the sole programmer on the team, working with 3 pixel artists, a story writer, an illustrator, and composer.`,
+    imgSrc: '/static/images/advent/advent-illustration.jpeg',
+    href: 'https://twitter.com/ReaperAdvent',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Eddie Bowles Historical Tour',
+    description: `Pokemon Go-esque mobile walking tour that explores the history of an underappreciated blues musician and composer from Cedar Falls, Iowa.`,
+    imgSrc: '/static/images/eddie-bowles/eddie-truck.jpg',
+    href: '/tags/eddie-bowles',
+  },
+  {
+    title: 'Runstrike',
+    description: `Augmented Reality running game. Gather resources on your routes, then return and progress through a retrofuturistic choose your own adventure story.`,
+    imgSrc: '/static/images/runstrike/runstrike-middle-of-splash.png',
+    href: '/tags/runstrike',
   },
 ]
+
+// TODO gru, spare change cycling
 
 export default projectsData
